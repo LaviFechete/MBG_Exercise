f9ed65e51bdbdab5b17cb9ca88f55d142b6a0d0f
LaviFechete <77480547+LaviFechete@users.noreply.github.com>
2023-09-12 14:48:16 +0200

    Update RScript.R

--- RScript.R
+++ RScript.R
@@ -1,10 +1,11 @@
-.libPaths(c("/content/miniconda/envs/myenv/lib/R/library/", .libPaths()))
+.libPaths(c("/content/miniconda/envs/myenv/lib/R/library/", .libPaths())) 
 
-library(ggplot2)
-library(ChIPseeker)
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+suppressMessages(library(ggplot2))
+suppressMessages(library(ChIPseeker))
+suppressMessages(library(TxDb.Hsapiens.UCSC.hg38.knownGene))
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 
+message("Loading data...")
 TBP <- read.delim("/content/macs3_results/TBP_summits.bed", header=F)
 TBP$V1 <- "chr1"
 NF_YA <- read.delim("/content/macs3_results/NF-YA_summits.bed", header=F)
@@ -14,22 +15,25 @@ write.table(NF_YA, "/content/macs3_results/NF-YA_renamed.bed", sep="\t", row.nam
 samplefiles <- list("/content/macs3_results/TBP_renamed.bed", "/content/macs3_results/NF-YA_renamed.bed")
 names(samplefiles) <- c("TBP", "NF_YA")
 
-
+message("Plot peaks...")
 p1<- covplot(samplefiles$TBP, weightCol="V5", title = "ChIP Peaks over region - TBP")
-ggsave("covplot_TBP.png", plot = p1)
+suppressMessages(ggsave("covplot_TBP.png", plot = p1, width=20, height=5, dpi=300))
 
 p2 <- covplot(samplefiles$NF_YA, weightCol="V5", title = "ChIP Peaks over region - NF-YA")
-ggsave("covplot_NFYA.png", plot = p2)
+suppressMessages(ggsave("covplot_NFYA.png", plot = p2, width=20, height=5, dpi=300))
 
-
+message("Plot Venn diagram...")
 peakAnnoList <- lapply(samplefiles, annotatePeak, TxDb=txdb, 
                        tssRegion=c(-3000, 3000), verbose=FALSE)
 
 p3 <-plotAnnoBar(peakAnnoList)
-ggsave("plotAnnoBar.png", plot = p3)
+suppressMessages(ggsave("plotAnnoBar.png", plot = p3))
 
-genes= lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
+genes = lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
 png("vennplot.png")
 vennplot(genes)
-dev.off()
+trash <- dev.off()
+
+message("Done!")
+
 
